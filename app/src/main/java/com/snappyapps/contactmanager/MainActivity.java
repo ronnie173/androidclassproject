b09652b50541be7c1da572a1c99283356d07a69b
Jerome Raymond <jerome.raymond@bottlerocketstudios.com>
2016-04-10 03:41:03 -0500

    Initial commit

---
+++ app/src/main/java/com/snappyapps/contactmanager/MainActivity.java
@@ -0,0 +1,76 @@
+package com.snappyapps.contactmanager;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.snappyapps.contactmanager.models.Contacts;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class MainActivity extends AppCompatActivity {
+    private Realm database;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        database = Realm.getInstance(new RealmConfiguration.Builder(this)
+        .name("database.realm")
+        .build());
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void insertData(String name,String address,String emailAddress,String image) {
+        database.beginTransaction();
+        //create an object
+        Contacts contacts = database.createObject(Contacts.class);
+        //set the fields
+        contacts.setName(name);
+        contacts.setAddress(address);
+        contacts.setEmailAddress(emailAddress);
+        contacts.setImage(image);
+        database.commitTransaction();
+        Toast.makeText(MainActivity.this, "Object inserted successfully", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/snappyapps/contactmanager/models/Contacts.java
@@ -0,0 +1,73 @@
+package com.snappyapps.contactmanager.models;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by jeromeraymond on 4/10/16.
+ */
+public class Contacts extends RealmObject {
+    @PrimaryKey
+    private int id;
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String emailAddress;
+    private String image;
+
+    public Contacts() {}
+
+
+    @Override
+    public void removeFromRealm() {
+        super.removeFromRealm();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
