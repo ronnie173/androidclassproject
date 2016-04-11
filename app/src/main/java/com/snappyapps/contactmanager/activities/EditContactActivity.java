fbb52d5db4e49491aef8585bc9362d7ab9790103
Jerome Raymond <ronnie173@gmail.com>
2016-04-11 14:00:08 -0500

    Added edit and add views

--- app/src/main/java/com/snappyapps/contactmanager/activities/EditContactActivity.java
+++ app/src/main/java/com/snappyapps/contactmanager/activities/EditContactActivity.java
@@ -1,6 +1,5 @@
 package com.snappyapps.contactmanager.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,27 +7,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-
 import com.snappyapps.contactmanager.R;
 
-public class ShowContactActivity extends AppCompatActivity {
+public class EditContactActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_contact);
+        setContentView(R.layout.activity_edit_contact2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), EditContactActivity.class);
-                view.getContext().startActivity(intent);
-            }
-        });
-    }
 
+    }
 
 }
